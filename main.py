65e7658a7ab4808f99e67b1660ad4cec88c5fabc
SalemNull <135310007+SalemNull@users.noreply.github.com>
2024-02-05 00:05:09 -0800

    Update main.py
    forgot to commit rewrite - added logging settings and a lot more control over bot init

--- main.py
+++ main.py
@@ -1,146 +1,206 @@
 import discord
 import os
-import random
 import logging
-import asyncio
+import platform
 
-from dotenv import find_dotenv, load_dotenv, set_key, get_key
-from discord.ext import commands, tasks
-from aiohttp import ClientError, ClientSession
-from datetime import datetime
+from dotenv import load_dotenv
+from discord.ext import commands
+from discord.ext.commands import Context
 
-from accuweather import (
-    AccuWeather,
-    ApiError,
-    InvalidApiKeyError,
-    InvalidCoordinatesError,
-    RequestsExceededError,
+
+
+class _ColourFormatter(logging.Formatter):
+    # ANSI codes are a bit weird to decipher if you're unfamiliar with them, so here's a refresher
+    # It starts off with a format like \x1b[XXXm where XXX is a semicolon separated list of commands
+    # The important ones here relate to colour.
+    # 30-37 are black, red, green, yellow, blue, magenta, cyan and white in that order
+    # 40-47 are the same except for the background
+    # 90-97 are the same but "bright" foreground
+    # 100-107 are the same as the bright ones but for the background.
+    # 1 means bold, 2 means dim, 0 means reset, and 4 means underline.
+
+    LEVEL_COLOURS = [
+        (logging.DEBUG, '\x1b[40;1m'),
+        (logging.INFO, '\x1b[34;1m'),
+        (logging.WARNING, '\x1b[33;1m'),
+        (logging.ERROR, '\x1b[31m'),
+        (logging.CRITICAL, '\x1b[41m'),
+    ]
+
+    FORMATS = {
+        level: logging.Formatter(
+            f'\x1b[33;1m%(asctime)s\x1b[0m {colour}%(levelname)-8s\x1b[0m \x1b[35m%(name)s\x1b[0m %(message)s',
+            '%m-%d %H:%M',
+        )
+        for level, colour in LEVEL_COLOURS
+    }
+
+    def format(self, record):
+        formatter = self.FORMATS.get(record.levelno)
+        if formatter is None:
+            formatter = self.FORMATS[logging.DEBUG]
+
+        # Override the traceback to always print in red
+        if record.exc_info:
+            text = formatter.formatException(record.exc_info)
+            record.exc_text = f'\x1b[31m{text}\x1b[0m'
+        output = formatter.format(record)
+
+        # Remove the cache layer
+        record.exc_text = None
+        return output
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Console handler
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(_ColourFormatter())
+# File handler
+file_handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+file_handler_formatter = logging.Formatter(
+    "[{asctime}] [{levelname:<8}] {name}: {message}", "%m-%d %H:%M", style="{"
 )
+file_handler.setFormatter(file_handler_formatter)
 
-# GLOBALS
-MAX_LOC = 2
-LOC_CODE = {}
-FORECAST_FREQ = 18
-DAILY = []
-DEG = "\N{DEGREE SIGN}"
-
-# Load file path for dotenv 
-env_file = find_dotenv()
-load_dotenv()
+# Add the handlers
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
 
 
-for i in range(MAX_LOC):
-    key = get_key(env_file, f"LOC{i}")
-    if key:
-        LOC_CODE[key] = get_key(env_file, f"NAME{i}")
+class DiscordBot(commands.Bot):
+    def __init__(self) -> None:
+        super().__init__(
+            command_prefix="!",
+            intents=discord.Intents.all(),
+            log_handler=None,
+            #log_formatter=None,
+            #log_level=(1),
+            #root_logger=True,
+            #help_command=None,
+        )
+        """
+        This creates custom bot variables so that we can access these variables in cogs more easily.
 
-print(f"Location code list: {LOC_CODE}")
+        For example, The config is available using the following code:
+        - self.config # In this class
+        - bot.config # In this file
+        - self.bot.config # In cogs
+        """
+        self.logger = logger
 
-#TODO: command to add a new location to .env 
-# set_key(env_file, f"LOC{i}", f"{new_loc_code}")
+    async def load_cogs(self) -> None:
+        """
+        The code in this function is executed whenever the bot will start.
+        """
+        logging.info(f"Logged in as {self.user.name}")
+        self.logger.info(f"discord.py API version: {discord.__version__}")
+        self.logger.info(f"Python version: {platform.python_version()}")
+        self.logger.info(
+            f"Running on: {platform.system()} {platform.release()} ({os.name})"
+        )
+        self.logger.info("-------------------")
 
+        for file in os.listdir('./cogs'):
+            if file.endswith(".py"):
+                extension = file[:-3]
+                try:
+                    await self.load_extension(f"cogs.{extension}")
+                    self.logger.info(f"Loaded extension '{extension}'")
+                except Exception as e:
+                    self.logger.exception(e)
 
-# Create a Discord client instance and set the command prefix
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='!', intents=intents)
+    async def setup_hook(self) -> None:
+        """
+        This will just be executed when the bot starts the first time.
+        """
+        await self.load_cogs()
 
-# Set the confirmation message when the bot is ready
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.name}')
-    
-    # list will be True if it has any elements.
-    if LOC_CODE:
-        update_topic.start()
+    async def on_message(self, message: discord.Message) -> None:
+        """
+        The code in this event is executed every time someone sends a message, with or without the prefix
 
+        :param message: The message that was sent.
+        """
+        if message.author == self.user or message.author.bot:
+            return
+        await self.process_commands(message)
 
-@bot.command()
-async def greet(ctx):
-    response = f'Hello {ctx.author.display_name}, I am your discord bot'
-    await ctx.send(response)
+    async def on_command_completion(self, ctx: Context) -> None:
+        """
+        The code in this event is executed every time a normal command has been *successfully* executed.
 
+        :param ctx: The context of the command that has been executed.
+        """
+        full_command_name = ctx.command.qualified_name
+        split = full_command_name.split(" ")
+        executed_command = str(split[0])
+        if ctx.guild is not None:
+            self.logger.info(
+                f"Executed {executed_command} command in {ctx.guild.name} by {ctx.author} (ID: {ctx.author.id})"
+            )
+        else:
+            self.logger.info(
+                f"Executed {executed_command} command by {ctx.author} (ID: {ctx.author.id}) in DMs"
+            )
 
-#TODO: eventually change from time interval to specific times for updating current conditions & daily forecasts
-@tasks.loop(minutes=65)
-async def update_topic():
-    print("Starting Update")
-    channel = bot.get_channel(int(os.getenv('CHANNEL_ID')))
-    api_key = os.getenv('ACCU_API')
-    update_forecast = not (update_topic.current_loop % FORECAST_FREQ)
+    async def on_command_error(self, ctx: Context, error) -> None:
+        """
+        The code in this event is executed every time a normal valid command catches an error.
 
-    if update_forecast: #TODO : and check requests remaining was not already 0 - i.e. keep old data
-        global DAILY
-        DAILY = []
-                    
-
-    report = await get_weather(api_key, update_forecast)
-
-    if report:
-        for item in DAILY:
-            report += f"\n{item}"
-
-        print(f"------FINAL REPORT IS ------\n{report}\n")
-        await channel.edit(topic=report)
-
-    else:
-        print("Error returned")
-    
-
-# Use Accuweather API to create initial objects for current conditions & forcasts
-async def get_weather(api_key, update_forecast):
-    current_summary = ""
-    requests_remaining = 0
-    print(f"update_forecast within function is: {update_forecast}")
-
-    for location, name in LOC_CODE.items():
-
-        async with ClientSession() as websession:
-            try:
-                weather = AccuWeather(
-                    api_key,
-                    websession,
-                    location_key=location,
-                    language="en",
+        :param ctx: The context of the normal command that failed executing.
+        :param error: The error that has been faced.
+        """
+        if isinstance(error, commands.CommandOnCooldown):
+            minutes, seconds = divmod(error.retry_after, 60)
+            hours, minutes = divmod(minutes, 60)
+            hours = hours % 24
+            embed = discord.Embed(
+                description=f"**Please slow down** - You can use this command again in {f'{round(hours)} hours' if round(hours) > 0 else ''} {f'{round(minutes)} minutes' if round(minutes) > 0 else ''} {f'{round(seconds)} seconds' if round(seconds) > 0 else ''}.",
+                color=0xE02B2B)
+            await ctx.send(embed=embed)
+        elif isinstance(error, commands.NotOwner):
+            embed = discord.Embed(
+                description="This is an admin-only command.",
+                color=0xE02B2B)
+            await ctx.send(embed=embed)
+            if ctx.guild:
+                self.logger.warning(
+                    f"{ctx.author} (ID: {ctx.author.id}) tried to execute an admin-only command in the guild {ctx.guild.name}."
                 )
-                # Get current weather conditions for this location
-                current_conditions = await weather.async_get_current_conditions()
-
-                # If a long duration has passed update the daily forecast 
-                if update_forecast:
-                    forecast_daily = await weather.async_get_daily_forecast(days=5, metric=False)
-                    print("updated daily forecast")
-            except (
-                ApiError,
-                InvalidApiKeyError,
-                InvalidCoordinatesError,
-                ClientError,
-                RequestsExceededError,
-            ) as error:
-                print(f"Error: {error}")
-                return None
-
             else:
-                requests_remaining = weather.requests_remaining
-                temperature = int(current_conditions['RealFeelTemperature']['Imperial']['Value'])
-                description = current_conditions['WeatherText']
-                current_summary += f"{name}: {temperature}{DEG} - {description}\n"
-                
-                if update_forecast:
-                    global DAILY
-                    entry = f"{name}: \n"
+                self.logger.warning(
+                    f"{ctx.author} (ID: {ctx.author.id}) tried to execute an admin-only command in the bot's DMs."
+                )
+        elif isinstance(error, commands.MissingPermissions):
+            embed = discord.Embed(
+                description="You are missing the permission(s) `"
+                + ", ".join(error.missing_permissions)
+                + "` to execute this command!",
+                color=0xE02B2B,
+            )
+            await ctx.send(embed=embed)
+        elif isinstance(error, commands.BotMissingPermissions):
+            embed = discord.Embed(
+                description="I am missing the permission(s) `"
+                + ", ".join(error.missing_permissions)
+                + "` to fully perform this command!",
+                color=0xE02B2B,
+            )
+            await ctx.send(embed=embed)
+        elif isinstance(error, commands.MissingRequiredArgument):
+            embed = discord.Embed(
+                title="Error!",
+                # We need to capitalize because the command arguments have no capital letter in the code and they are the first word in the error message.
+                description=str(error).capitalize(),
+                color=0xE02B2B,
+            )
+            await ctx.send(embed=embed)
+        else:
+            raise error
 
-                    for day in forecast_daily[:3]:
-                        date = day['Date'][5:10]
-                        maximum = int(day['TemperatureMax']['Value'])
-                        minimum = int(day['TemperatureMin']['Value'])
-                        entry += f"{date}: [H: {maximum}{DEG}  L: {minimum}{DEG}]\n"
-                        
-                        
-                    DAILY.append(entry)
 
-    current_summary += f"\n{requests_remaining} API calls remaining - updated {datetime.now().strftime('%H:%M')}\n"
-    return current_summary
-
-
-# Retrieve token from the .env file
-bot.run(os.getenv('TOKEN'))
+load_dotenv()
+bot = DiscordBot()
+bot.run(os.getenv("TOKEN"))
